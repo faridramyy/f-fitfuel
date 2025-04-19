261e44f5736564767e6de05811baa303add3a041
farid ramy <faridramy2003@gmail.com>
2025-04-19 22:17:43 +0200

    install firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fitfuel/firebase_options.dart';
 import 'package:fitfuel/theme/app_theme.dart';
 import 'package:fitfuel/routes/app_router.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
